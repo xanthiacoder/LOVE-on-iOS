7c50583fc6176abf95de05b5730b7d4d2bc1c86b
Joash Chee <joash@scorelessmusic.com>
2025-09-10 12:34:00 +0800

    Patched conf.lua for 11.5 => 12.0

--- input/Screen-Test-iPad/conf.lua
+++ input/Screen-Test-iPad/conf.lua
@@ -3,9 +3,7 @@ function love.conf(t)
     t.appendidentity = false             -- Search files in source directory before save directory (boolean)
     t.version = "12.0"                   -- The LÖVE version this game was made for (string)
     t.console = false                    -- Attach a console (boolean, Windows only)
-    t.accelerometerjoystick = true       -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
     t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean) 
-    t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
 
     t.audio.mic = false                 -- Request and use microphone capabilities in Android (boolean)
     t.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
@@ -24,8 +22,6 @@ function love.conf(t)
     t.window.msaa = 0                   -- The number of samples to use with multi-sampled antialiasing (number)
     t.window.depth = nil                -- The number of bits per sample in the depth buffer
     t.window.stencil = nil              -- The number of bits per sample in the stencil buffer
-    t.window.display = 1                -- Index of the monitor to show the window in (number)
-    t.window.highdpi = false            -- Enable high-dpi mode for the window on a Retina display (boolean)
     t.window.usedpiscale = true         -- Enable automatic DPI scaling when highdpi is set to true as well (boolean)
     t.window.x = nil                    -- The x-coordinate of the window's position in the specified display (number)
     t.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number)
